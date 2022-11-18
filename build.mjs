d66de31c33ea0a421f6c5e3687602b902ec3ef41
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-11-18 11:44:50 +0100

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -8,6 +8,7 @@
 
 import * as esbuild from 'esbuild';
 import { execSync } from 'child_process';
+import * as fs from 'fs';
 
 // ---- build ----
 esbuild.buildSync({
@@ -21,12 +22,37 @@ esbuild.buildSync({
 });
 
 // ---- convert README.md to README.html ----
-const date = new Date().toISOString().slice(0, 10);
+/*const date = new Date().toISOString().slice(0, 10);
 execSync("sed -e '1,/<!-- start-for-website -->/d' README.md > __tmp.md");
 execSync(
   'pandoc -s __tmp.md --metadata title="MATHE:BUDDY SIMULATOR" --metadata author="" --metadata date="' +
     date +
-    '"  --css README.css --self-contained -o README.html',
+    '"  --css README.css --embed-resources --standalone -o README.html',
 );
 execSync('rm __tmp.md');
 // TODO: --mathjax may be needed, but results in large file, if --self-contained option is provided...
+*/
+
+// ---- convert README.md to README.html ----
+
+const date = new Date().toISOString().slice(0, 10);
+execSync("sed -e '1,/<!-- start-for-website -->/d' README.md > __tmp.md");
+execSync(
+  'pandoc -s __tmp.md --metadata title="Mathe:Buddy Simulator Install Guide" --metadata author="" --metadata date="' +
+    date +
+    '"  --css README.css --embed-resources --standalone -o sim.html',
+);
+execSync('rm __tmp.md');
+
+// TODO: --mathjax may be needed, but results in large file, if --self-contained option is provided...
+
+const title = `<h1 class="title">`;
+const links =
+  `[<a href="https://app.f07-its.fh-koeln.de">Home</a>] ` +
+  `[<a href="https://app.f07-its.fh-koeln.de/docs-mbl.html">MBL Reference</a>] ` +
+  `[<a href="https://app.f07-its.fh-koeln.de/docs-smpl.html">SMPL Reference</a>] ` +
+  `[<a href="https://app.f07-its.fh-koeln.de/docs-sim.html">Installation</a>] `;
+fs.writeFileSync(
+  'sim.html',
+  fs.readFileSync('sim.html', 'utf-8').replace(title, links + title),
+);
