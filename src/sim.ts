80b10b4e94c0984466ada16653f272dbbbf3bc7f
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-12-16 13:37:28 +0100

    Update sim.ts

--- src/sim.ts
+++ src/sim.ts
@@ -10,15 +10,42 @@ import {
   MBL_Course,
   MBL_Course_Debug,
 } from '@mathebuddy/mathebuddy-compiler/src/dataCourse';
-import { MBL_Exercise } from '@mathebuddy/mathebuddy-compiler/src/dataExercise';
-import { MBL_Level } from '@mathebuddy/mathebuddy-compiler/src/dataLevel';
+import { MBL_Definition } from '@mathebuddy/mathebuddy-compiler/src/dataDefinition';
+import { MBL_Equation } from '@mathebuddy/mathebuddy-compiler/src/dataEquation';
+import { MBL_Error } from '@mathebuddy/mathebuddy-compiler/src/dataError';
+import {
+  MBL_Exercise,
+  MBL_Exercise_Text_Multiple_Choice,
+  MBL_Exercise_Text_Variable,
+  MBL_Exercise_VariableType,
+} from '@mathebuddy/mathebuddy-compiler/src/dataExercise';
+import {
+  MBL_Level,
+  MBL_LevelItem,
+} from '@mathebuddy/mathebuddy-compiler/src/dataLevel';
+import { MBL_Section } from '@mathebuddy/mathebuddy-compiler/src/dataSection';
+import { MBL_Table } from '@mathebuddy/mathebuddy-compiler/src/dataTable';
+import {
+  MBL_Text,
+  MBL_Text_AlignCenter,
+  MBL_Text_Bold,
+  MBL_Text_Color,
+  MBL_Text_InlineMath,
+  MBL_Text_Itemize,
+  MBL_Text_Paragraph,
+  MBL_Text_Span,
+  MBL_Text_Text,
+} from '@mathebuddy/mathebuddy-compiler/src/dataText';
 
 import { MathJax } from './mathjax';
-import { matrix2tex } from './tex';
+import { matrix2tex, set2tex, term2tex } from './tex';
 
 export class Simulator {
   private course: MBL_Course = null;
+
   private exercise: MBL_Exercise = null;
+  private exerciseInstanceIndex = 0;
+
   private mathjaxInst: MathJax = null;
 
   private parentDOM: HTMLElement = null;
@@ -51,16 +78,12 @@ export class Simulator {
 
   public generateDOM(): boolean {
     this.parentDOM.innerHTML = '';
-
-    const h4 = document.createElement('h4');
-    h4.innerHTML = 'SIMULATOR WILL BE AVAILABLE SOON!!';
-
     switch (this.course.debug) {
       case MBL_Course_Debug.Level:
         this.parentDOM.appendChild(
           this.generateLevel(this.course.chapters[0].levels[0]),
         );
-        this.parentDOM.appendChild(h4);
+        //this.parentDOM.appendChild(h4);
         break;
       default:
         console.log(
@@ -71,30 +94,339 @@ export class Simulator {
     }
 
     return true;
-
-    /*// if no alias is provided, then use the first document
-    if (documentAlias.length == 0) {
-      if (this.course.documents.length == 0) return false;
-      this.parentDOM.appendChild(this.genDoc(this.course.documents[0]));
-      return true;
-    }
-    // otherwise search for document with alias
-    else {
-      for (const doc of this.course.documents) {
-        if (doc.alias === documentAlias) {
-          this.parentDOM.appendChild(this.genDoc(doc));
-          //console.log(this.parentDOM.innerHTML);
-          return true;
-        }
-      }
-    }
-    return false;*/
   }
 
   private generateLevel(level: MBL_Level): HTMLElement {
-    const span = document.createElement('span');
-    span.innerHTML = level.title;
-    return span;
+    const root = document.createElement('div');
+    // title
+    const title = document.createElement('h4');
+    title.innerHTML = level.title.toUpperCase();
+    title.classList.add('text-center', 'py-2');
+    root.appendChild(title);
+    // level items
+    for (const item of level.items) {
+      const itemHTML = this.generateLevelItem(item);
+      if (itemHTML != null) root.appendChild(itemHTML);
+    }
+    return root;
+  }
+
+  private generateLevelItem(item: MBL_LevelItem): HTMLElement {
+    switch (item.type) {
+      case 'section': {
+        const section = <MBL_Section>item;
+        const element = document.createElement('h1');
+        element.innerHTML = section.text;
+        return element;
+      }
+      case 'subsection': {
+        const section = <MBL_Section>item;
+        const element = document.createElement('h2');
+        element.innerHTML = section.text;
+        return element;
+      }
+      case 'subsubsection': {
+        const section = <MBL_Section>item;
+        const element = document.createElement('h3');
+        element.innerHTML = section.text;
+        return element;
+      }
+      case 'equation': {
+        const equation = <MBL_Equation>item;
+        const element = document.createElement('div');
+        element.classList.add('text-center');
+        let tex = equation.value;
+        if (equation.numbering > 0) tex += '~~(' + equation.numbering + ')';
+        const html = this.mathjaxInst.tex2svgBlock(tex);
+        element.innerHTML = ' ' + html + ' ';
+        return element;
+      }
+      case 'definition':
+      case 'theorem': {
+        const definition = <MBL_Definition>item;
+        const element = document.createElement('div');
+        element.classList.add('my-1', 'p-1');
+        element.classList.add('border', 'border-dark');
+        for (const subItem of definition.items) {
+          element.appendChild(this.generateLevelItem(subItem));
+        }
+        return element;
+      }
+      case 'exercise': {
+        this.exercise = <MBL_Exercise>item;
+        this.exerciseInstanceIndex = Math.floor(
+          Math.random() * this.exercise.instances.length,
+        );
+        const element = document.createElement('div');
+        const title = document.createElement('h4');
+        element.appendChild(title);
+        title.classList.add('text-center');
+        title.innerHTML = this.exercise.title;
+        element.classList.add('my-1', 'p-1');
+        element.appendChild(this.generateTextItem(this.exercise.text));
+        return element;
+      }
+      case 'table': {
+        const table = <MBL_Table>item;
+        const element = document.createElement('div');
+        const p = document.createElement('p');
+        p.classList.add('text-center');
+        p.innerHTML = table.title;
+        element.appendChild(p);
+        const tableElement = document.createElement('table');
+        element.appendChild(tableElement);
+        tableElement.classList.add('table');
+        if (table.head.columns.length > 0) {
+          const thead = document.createElement('thead');
+          tableElement.appendChild(thead);
+          for (const column of table.head.columns) {
+            const th = document.createElement('th');
+            th.scope = 'col';
+            thead.appendChild(th);
+            th.appendChild(this.generateTextItem(column));
+          }
+        }
+        const tbody = document.createElement('tbody');
+        tableElement.appendChild(tbody);
+        for (const row of table.rows) {
+          const tr = document.createElement('tr');
+          tbody.appendChild(tr);
+          for (const column of row.columns) {
+            const td = document.createElement('td');
+            tr.appendChild(td);
+            td.appendChild(this.generateTextItem(column));
+          }
+        }
+        return element;
+      }
+      case 'paragraph':
+      case 'align_center': {
+        return this.generateTextItem(item);
+      }
+      default:
+        console.log(
+          'warning:generateLevelItem(..): unimplemented type: ' + item.type,
+        );
+    }
+    return document.createElement('span');
+  }
+
+  private generateTextItem(item: MBL_Text): HTMLElement {
+    switch (item.type) {
+      case 'paragraph': {
+        const paragraph = <MBL_Text_Paragraph>item;
+        const p = document.createElement('p');
+        for (const paragraphItem of paragraph.items)
+          p.appendChild(this.generateTextItem(paragraphItem));
+        return p;
+      }
+      case 'align_left':
+      case 'align_center':
+      case 'align_right': {
+        const align = <MBL_Text_AlignCenter>item;
+        const element = document.createElement('div');
+        switch (item.type) {
+          case 'align_left':
+            element.classList.add('text-start');
+            break;
+          case 'align_center':
+            element.classList.add('text-center');
+            break;
+          case 'align_right':
+            element.classList.add('text-end');
+            break;
+        }
+        for (const subItem of align.items)
+          element.appendChild(this.generateTextItem(subItem));
+        return element;
+      }
+      case 'text': {
+        const text = <MBL_Text_Text>item;
+        const element = document.createElement('span');
+        element.innerHTML = text.value;
+        return element;
+      }
+      case 'linefeed': {
+        const span = document.createElement('span');
+        span.innerHTML = '<br/><br/>';
+        return span;
+      }
+      case 'span': {
+        const span = <MBL_Text_Span>item;
+        const element = document.createElement('span');
+        for (const subItem of span.items)
+          element.appendChild(this.generateTextItem(subItem));
+        return element;
+      }
+      case 'bold': {
+        const bold = <MBL_Text_Bold>item;
+        const element = document.createElement('strong');
+        element.classList.add('px-1');
+        for (const subItem of bold.items)
+          element.appendChild(this.generateTextItem(subItem));
+        return element;
+      }
+      case 'italic': {
+        const italic = <MBL_Text_Bold>item;
+        const element = document.createElement('em');
+        element.classList.add('px-1');
+        for (const subItem of italic.items)
+          element.appendChild(this.generateTextItem(subItem));
+        return element;
+      }
+      case 'color': {
+        const color = <MBL_Text_Color>item;
+        const element = document.createElement('span');
+        element.classList.add('px-1');
+        switch (color.key) {
+          case 0:
+            element.style.color = 'rgb(0,0,0)';
+            break;
+          case 1:
+            element.style.color = 'rgb(255,0,0)';
+            break;
+          case 2:
+            element.style.color = 'rgb(0,0,255)';
+            break;
+          case 3:
+            element.style.color = 'rgb(0,255,0)';
+            break;
+          default:
+            console.log(
+              'warning:generateTextItem(..): unimplemented color key: ' +
+                color.key,
+            );
+        }
+        for (const subItem of color.items)
+          element.appendChild(this.generateTextItem(subItem));
+        return element;
+      }
+      case 'itemize':
+      case 'enumerate':
+      case 'enumerate_alpha': {
+        const itemize = <MBL_Text_Itemize>item;
+        let element: HTMLUListElement | HTMLOListElement;
+        switch (item.type) {
+          case 'itemize':
+            element = document.createElement('ul');
+            break;
+          case 'enumerate':
+            element = document.createElement('ol');
+            break;
+          case 'enumerate_alpha':
+            element = document.createElement('ol');
+            element.type = 'a';
+            break;
+        }
+        for (const subItem of itemize.items) {
+          const li = document.createElement('li');
+          element.appendChild(li);
+          li.appendChild(this.generateTextItem(subItem));
+        }
+        return element;
+      }
+      case 'inline_math': {
+        const inlineMath = <MBL_Text_InlineMath>item;
+        const element = document.createElement('span');
+        element.classList.add('m-1');
+        let tex = '';
+        for (const subItem of inlineMath.items) {
+          switch (subItem.type) {
+            case 'text': {
+              const text = <MBL_Text_Text>subItem;
+              tex += text.value;
+              break;
+            }
+            case 'variable': {
+              const variable = <MBL_Exercise_Text_Variable>subItem;
+              const v = this.exercise.variables[variable.variableId];
+              const value =
+                this.exercise.instances[this.exerciseInstanceIndex].values[
+                  variable.variableId
+                ];
+              switch (v.type) {
+                case MBL_Exercise_VariableType.Bool:
+                case MBL_Exercise_VariableType.Int:
+                case MBL_Exercise_VariableType.Real:
+                case MBL_Exercise_VariableType.Complex:
+                  tex += value;
+                  break;
+                case MBL_Exercise_VariableType.IntSet:
+                case MBL_Exercise_VariableType.RealSet:
+                  tex += set2tex(value);
+                  break;
+                case MBL_Exercise_VariableType.Matrix:
+                  tex += matrix2tex(value);
+                  break;
+                case MBL_Exercise_VariableType.Term:
+                  tex += term2tex(value);
+                  break;
+                default:
+                  console.log(
+                    'warning:generateTextItem(..):inline_math:variable: ' +
+                      'unimplemented type: ' +
+                      v.type,
+                  );
+              }
+              break;
+            }
+            default:
+              console.log(
+                'warning:generateTextItem(..):inline_math: unimplemented type: ' +
+                  subItem.type,
+              );
+          }
+        }
+        const html = this.mathjaxInst.tex2svgInline(tex);
+        element.innerHTML = ' ' + html + ' ';
+        return element;
+      }
+      case 'text_input': {
+        // TODO: depends on type, ...
+        const element = document.createElement('input');
+        element.classList.add('m-1');
+        return element;
+      }
+      case 'multiple_choice': {
+        const mc = <MBL_Exercise_Text_Multiple_Choice>item;
+        const element = document.createElement('table');
+        // todo: randomize order
+        for (const option of mc.items) {
+          const tr = document.createElement('tr');
+          tr.classList.add('p-1');
+          element.appendChild(tr);
+          // check box
+          let td = document.createElement('td');
+          td.classList.add('p-1');
+          tr.appendChild(td);
+          const input = document.createElement('input');
+          input.classList.add('form-check-input');
+          input.type = 'checkbox';
+          input.value = '';
+          td.appendChild(input);
+          // text
+          td = document.createElement('td');
+          td.classList.add('p-1');
+          tr.appendChild(td);
+          td.appendChild(this.generateTextItem(option.text));
+        }
+        return element;
+      }
+      case 'error': {
+        const error = <MBL_Error>item;
+        const element = document.createElement('div');
+        element.classList.add('text-danger', 'border', 'border-dark');
+        const p = document.createElement('p');
+        element.appendChild(p);
+        p.innerHTML = error.message;
+        return element;
+      }
+      default:
+        console.log(
+          'warning:generateTextItem(..): unimplemented type: ' + item.type,
+        );
+    }
+    return document.createElement('span');
   }
 
   /*private genDoc(doc: Doc): HTMLElement {
