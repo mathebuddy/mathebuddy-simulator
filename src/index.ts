51fbb4a7410401399b8df314e763890512f5ae1b
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-12-21 13:29:01 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,33 +11,49 @@ import { MBL_Course } from '@mathebuddy/mathebuddy-compiler/src/dataCourse';
 
 import { Simulator } from './sim';
 
+let compilerLog = '';
+
 export function compile(
   path: string,
   files: { [fileId: string]: string },
 ): MBL_Course {
+  compilerLog = '';
   function load(path: string): string {
     if (path in files) return files[path];
+    // TODO output warning
     else return '';
   }
   const compiler = new Compiler();
   try {
     compiler.compile(path, load);
   } catch (e) {
-    // TODO: error handling
-    console.log(e);
+    compilerLog = '' + e;
+    return null;
   }
   const course = compiler.getCourse();
   //console.log(course.toJSON());
   return course;
 }
 
-export function createSim(data: MBL_Course, root: HTMLElement): Simulator {
-  const sim = new Simulator(data, root);
+export function getCompilerLog(): string {
+  return compilerLog;
+}
+
+export function createSim(root: HTMLElement): Simulator {
+  const sim = new Simulator(root);
   return sim;
+}
+
+export function setCourse(sim: Simulator, course: MBL_Course): void {
+  sim.setCourse(course);
 }
 
 export function generateDOM(sim: Simulator): boolean {
   return sim.generateDOM();
+}
+
+export function getLOG(sim: Simulator): string {
+  return sim.getLog();
 }
 
 export function getJSON(sim: Simulator): string {
